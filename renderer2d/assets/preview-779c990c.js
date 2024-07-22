1ee781a67d5017c61ef7ba3f998918243ce61c28
ZhongxuYang <himatthew@foxmail.com>
2024-07-22 17:38:07 +0800

    Delete renderer2d directory

--- renderer2d/assets/preview-779c990c.js
+++
@@ -1,2 +0,0 @@
-const e={parameters:{options:{showPanel:!1},actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-779c990c.js.map
